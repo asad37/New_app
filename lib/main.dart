9c0d0452bb8ea81b09a5f0276a2f453e48355937
AsadWaqas <asadkambo2021@gmail.com>
2023-06-07 13:54:11 +0500

    Add Video

---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:my_app/home_screen.dart';
+
+import 'firebase_options.dart';
+import 'login_screen.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+);
+  runApp(const MyApp());
+}
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+       primarySwatch: Colors.blue
+      ),
+      home: const LoginScreen()
+    );
+  }
+}
+
